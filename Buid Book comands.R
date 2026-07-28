d70fd38cf7cf91895cf9da375c5b98623640c0bf
ashan.jayamal <ashan.jayamal@gmail.com>
2026-07-27 21:01:39 -0500

    update

--- Buid Book comands.R
+++ Buid Book comands.R
@@ -4,3 +4,10 @@ serve_book(dir = ".", output_dir = "_book", preview = TRUE,in_session = TRUE, qu
 
 # Buid Book
 rmarkdown::render_site(encoding = 'UTF-8')
+
+rmarkdown::clean_site()
+rmarkdown::clean_site(preview = FALSE)
+
+
+bookdown::render_book("index.Rmd", "bookdown::gitbook")   
+render_book(input = "index.Rmd", output_format = "bookdown::gitbook", clean = TRUE)
